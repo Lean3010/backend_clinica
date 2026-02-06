85482fe5eb1b0cc9a3fd0ad21e66f4ab2c02204b
Leandro Esper <leaesper2014@gmail.com>
2026-02-06 10:15:53 -0300

    termine de hacer el login

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ const cors = require('cors');
 const connectDB = require('./config/database');
 const morgan = require('morgan');
 
+// Importar los archivos de los enrutadores
+const authRoutes = require('./routes/auth.routes');
+
+
+
+
 // Conectar a la base de datos
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// RUTAS
+app.use('/api/auth', authRoutes);
+
+
+// PUERTO
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
