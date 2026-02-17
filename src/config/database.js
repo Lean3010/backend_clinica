02456e9e13f653fbb7077e0df8f516aa066a3a0b
Florencia Cabello <florenciaacabello@gmail.com>
2026-02-17 17:32:52 -0300

    quitando console log

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    console.log(process.env.MONGODB_URI)
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB Conectado Exitosamente');
