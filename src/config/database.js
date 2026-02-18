a514d4269755781bdc697a80e268b753637f2c33
Florencia Cabello <florenciaacabello@gmail.com>
2026-02-18 19:38:30 -0300

    subiendo la creacion automatica del admin

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,16 @@
 // Conexion a la base de datos
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const crearAdmin = require("./utils/crearAdmin");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Conectado Exitosamente');
-    } catch (error) {
-        console.error('Error conectando a MongoDB:', error);
-        process.exit(1); // Detiene la app si falla la DB
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Conectado Exitosamente");
+    await crearAdmin();
+  } catch (error) {
+    console.error("Error conectando a MongoDB:", error);
+    process.exit(1); // Detiene la app si falla la DB
+  }
 };
 
 module.exports = connectDB;
